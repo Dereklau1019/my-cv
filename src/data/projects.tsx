ee0bd5276b67705e49177918aea243ce84843604
Derek Lau <derek.lau@ptt-asia.com>
2025-02-13 03:57:36 +0800

    Upload : project & project company logo

---
+++ src/data/projects.tsx
@@ -0,0 +1,63 @@
+interface Project {
+  shortName: string;
+  fullName: string;
+  OfficialWebsite: string;
+  logoUrl: string;
+}
+
+export const projects: Project[] = [
+  {
+    shortName: "HKIA",
+    fullName: "Hong Kong International Airport",
+    OfficialWebsite: "https://www.hongkongairport.com/",
+    logoUrl: "/src/images/company/hkia_logo.png",
+  },
+  {
+    shortName: "CLP",
+    fullName: "China Light and Power Company, Limited",
+    OfficialWebsite: "https://www.clp.com.hk/zh/index",
+    logoUrl: "/src/images/company/clp_logo.png",
+  },
+  {
+    shortName: "MTR",
+    fullName: "Mass Transit Railway Corporation",
+    OfficialWebsite: "https://www.mtr.com.hk/en/customer/main/index.html",
+    logoUrl: "/src/images/company/mtr_logo.png",
+  },
+  {
+    shortName: "MLM",
+    fullName: "Macao Light Rapid Transit Corporation, Limited",
+    OfficialWebsite: "https://www.mlm.com.mo/en/index.html",
+    logoUrl: "/src/images/company/mlm_logo.png",
+  },
+  {
+    shortName: "HKJC",
+    fullName: "The Hong Kong Jockey Club",
+    OfficialWebsite: "https://www.hkjc.com/home/english/index.aspx",
+    logoUrl: "/src/images/company/hkjc_logo.png",
+  },
+  {
+    shortName: "Towngas",
+    fullName: "The Hong Kong and China Gas Company Limited",
+    OfficialWebsite: "https://www.towngas.com/en/",
+    logoUrl: "/src/images/company/towngas_logo.png",
+  },
+  {
+    shortName: "DSD",
+    fullName: "Drainage Services Department",
+    OfficialWebsite: "https://www.dsd.gov.hk/EN/Home/index.html",
+    logoUrl: "/src/images/company/dsd_logo.png",
+  },
+  {
+    shortName: "LINK REIT",
+    fullName: "Link Real Estate Investment Trust",
+    OfficialWebsite: "https://www.linkreit.com/en/",
+    logoUrl: "/src/images/company/link_reit_logo.png",
+  },
+  {
+    shortName: "VTC",
+    fullName: "The Vocational Training Council",
+    OfficialWebsite: "https://www.vtc.edu.hk/home/en/",
+    logoUrl: "/src/images/company/vtc_logo.png",
+  },
+];
